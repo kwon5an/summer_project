4c2dd8e7602655723e673e11d217bd12f2cb2dc4
Kwon5an <gykwon0821@gmail.com>
2025-07-30 00:22:47 +0900

    main함수 추가

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include "GameManager.cpp"
+using namespace std;
+
+int main() {
+    string name;
+	cout << "플레이어 이름을 입력하세요: ";
+	getline(cin, name);
+
+    GameManager game(name);
+    game.StartGame();
+
+    return 0;
+}
